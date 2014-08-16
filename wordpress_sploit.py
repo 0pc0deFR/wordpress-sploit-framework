7200940aabfb7838b3a268bb355047aa9de37885
0pc0deFR <0pc0deFR@gmail.com>
2014-08-16 11:11:37 +0200

    V0.1.1

--- wordpress_sploit.py
+++ wordpress_sploit.py
@@ -2,18 +2,4 @@ from exploit_configuration import *
 from urllib import *
 from argparse import *
 
-"""
-class version(Action):
-	def __call__(self, parser, namespace, values, option_string=None):
-		print 'ok'
-
-class uri(Action):
-	def __call__(self, parser, namespace, values, option_string=None):
-		print 'serveur'
-
-parser = ArgumentParser()
-parser.add_argument('S', action=uri, nargs=1, help='Definir le serveur cible')
-parser.add_argument('-P', action=version, nargs=1, help='Load your plugin')
-args = parser.parse_args()
-"""
 ExploitConfiguration('./exploits/WordPress_bib2html_0_9_3_Cross_Site_Scripting', './payloads/alert', 'http://serveur/wordpress-development/')
